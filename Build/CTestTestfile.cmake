c7cb0892b446ddb213ea34a8539702adc35d8393
Nestor <nestoroprysk@gmail.com>
2019-03-03 22:29:03 +0200

    Deleting autogen

--- Build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nestoroprysk/Documents/GitHub/JsonParser2
-# Build directory: /Users/nestoroprysk/Documents/GitHub/JsonParser2/Build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
